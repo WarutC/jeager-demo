86d0a89bb4dda11645a0a147598e7bcdba3efe29
Warut <warut.chm@gmail.com>
2025-05-15 03:10:41 +0700

    feat: restructure application by removing app.controller.spec.ts, updating app.controller.ts to include a root path, and adding data module with controller and service
    feat: integrate OpenTelemetry tracing and Swagger documentation in main.ts, and create tracing.ts for tracing configuration

--- nestjs-project-1/src/app.module.ts
+++ nestjs-project-1/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DataModule } from './data/data.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), 
+    DataModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ nestjs-project-1/src/tracing.ts
@@ -0,0 +1,48 @@
+'use strict';
+
+import { ConfigModule } from '@nestjs/config';
+import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core';
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
+
+// Load environment variables from the .env file
+ConfigModule.forRoot();
+
+// Configure the endpoints/paths to ignore when tracing incoming requests
+const IGNORED_PATHS = ['/', '/api', '/health'];
+
+// Initialize the SDK and register with the OpenTelemetry API
+const sdk = new NodeSDK({
+  traceExporter: new OTLPTraceExporter({
+    url: process.env.TRACING_AGENT_URL ?? 'http://localhost:4317', // (e.g. Jaeger -> http://localhost:4317)
+  }),
+  instrumentations: [
+    new NestInstrumentation(),
+    new HttpInstrumentation({
+      ignoreIncomingRequestHook: (request) => {
+        if (IGNORED_PATHS.includes(request.url ?? '')) {
+          return true;
+        }
+        return false;
+      },
+    }),
+    new ExpressInstrumentation(),
+  ],
+  serviceName: process.env.TRACING_SERVICE_NAME ?? 'service-a', // (e.g. 'my-service')  
+});
+
+// Enable the API to record telemetry
+sdk.start();
+
+// Gracefully shut down the SDK on process exit
+process.on('SIGTERM', () => {
+  sdk
+    .shutdown()
+    .then(() => console.log('Tracing terminated'))
+    .catch((error) => console.log('Error terminating tracing', error))
+    .finally(() => process.exit(0));
+});
+
+export default sdk;
